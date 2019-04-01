6f71fe93d0d076f0c8d79eb0ebbe4a5edb11793c
Andy Wood <andrewww@usc.edu>
2019-03-31 21:27:26 -0700

    Reverting last commit
    This reverts commit 79a6cbaeeaeb47fe05ff0c1860570186e28b785b.
    
    Reverting last commit.

--- javascript/search.js
+++ javascript/search.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     //$("#menu").hide();
     console.log("loaded");
     $("#hamburger-menu-img").click(function(){
+        alert("here");
         $("#menu").fadeIn(200);
     });
     $("#close-slide").click(function(){
